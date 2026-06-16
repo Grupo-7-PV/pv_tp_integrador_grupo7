c9c40f3bc93e1106c65d5b1e035ec2738fb5fb48
tuttu <lucassantiagomn@gmail.com>
2026-06-16 16:40:07 -0300

    context por terminar
    margin de prueba

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,20 +1,27 @@
 import { Container, Card, Form, Button } from 'react-bootstrap';
 import { useState } from 'react';
 import {login} from '../Services/authService.js';
+import {useAdmin} from '../hook/useAuth.js';
 
 const Login = () => {
     const [usuario, setUsuario] = useState('');
-    const [contraseña, setContraseña] = useState('');
+    const [password, setPassword] = useState('');
     const [sector, setSector] = useState('');
-    const handleSubmit = async (e) => { e.preventDefault();
-       
+    const {iniciarSesion} = useAdmin(); 
+    const handleSubmit = async (e) => { e.preventDefault();  
+    
         try {
-            const perfil = await login(usuario, contraseña, sector);
+            const perfil = await login(usuario, password, sector);
+             iniciarSesion(perfil);
+             
             console.log("Perfil del usuario:", perfil);
+          
         } catch (error) {
             console.error("Error al iniciar sesión:", error);
+        
         }
     };
+
     return (<Container
         className="d-flex align-items-center justify-content-center vh-100"
     >
@@ -38,8 +45,8 @@ const Login = () => {
                         <Form.Control type="password"
                             placeholder="contraseña"
                             style={{ borderRadius: "50px" }}
-                            value={contraseña}
-                            onChange={e => setContraseña(e.target.value)}>
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}>
                         </Form.Control>
                     </Form.Group>
                     <Form.Group className="mb-3">
