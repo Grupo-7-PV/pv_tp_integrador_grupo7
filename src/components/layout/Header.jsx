ac8e6bf18b5013f10f8b1fbea0cd92b78d63a94a
RFranciscoTolaba <franciscotolaba66@gmail.com>
2026-06-21 21:25:13 -0300

    Incorporación de Dashboard (se muestra en nav)

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,10 @@
-import {useNavigate} from 'react-router-dom';
-import {Navbar, Container, Button} from 'react-bootstrap';
-import {useAdmin} from '../../hook/useAdmin';
+import { useNavigate } from 'react-router-dom';
+import { Navbar, Container, Button, Nav } from 'react-bootstrap';
+import { useAdmin } from '../../hook/useAdmin';
 
 const Header = () => {
     const navigate = useNavigate();
-    const {adminActivo, cerrarSesion}=useAdmin();
+    const { adminActivo, cerrarSesion } = useAdmin();
 
     const handleLogout = () => {
         cerrarSesion();
@@ -16,12 +16,24 @@ const Header = () => {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="/">Sistema de Gestión</Navbar.Brand>
+                <Navbar.Brand href="/dashboard">Sistema de Gestión</Navbar.Brand>
+
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
 
                     {adminActivo && (
                         <>
+                            <Nav className="me-3">
+
+                                <Nav.Link onClick={() => navigate('/clientes')}>
+                                    Clientes
+                                </Nav.Link>
+
+                                <Nav.Link onClick={() => navigate('/dashboard')}>
+                                    Dashboard
+                                </Nav.Link>
+
+                            </Nav>
                             <Navbar.Text className="me-3 text-white">
                                 {adminActivo.nombre} | {adminActivo.sector}
                             </Navbar.Text>
