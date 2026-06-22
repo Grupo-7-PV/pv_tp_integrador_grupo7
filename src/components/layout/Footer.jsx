a2288f09351ec4755fbd09c5886673385675b66e
tuttu <lucassantiagomn@gmail.com>
2026-06-21 21:31:32 -0300

    footer agregado

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,23 @@
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+
+  return (
+
+    <footer className="bg-dark text-white text-center py-3 mt-auto">
+
+      <Container>
+
+        <p className="mb-1">Copyright © Grupo7. Todos los derechos reservados.</p>
+
+        <p className="mb-0">Francisco, Kevin, Ricardo, Lucas, Gerardo</p>
+
+      </Container>
+
+    </footer>
+
+  );
+
+};
+
+export default Footer;
